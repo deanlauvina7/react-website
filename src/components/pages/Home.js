d152b4e5442d779190c3bbc09e462e693a70e4c3
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-05 11:03:09 -0600

    Working on the cards

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HereSection';
+import Cards from '../Cards';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  );
+}
+
+export default Home;
