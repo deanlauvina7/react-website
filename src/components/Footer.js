e53d40008519f26ff98ebcff35105704611c38d9
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-05 19:01:25 -0600

    Creating the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button } from './Button';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -22,6 +24,95 @@ function Footer() {
             </form>
           </div>
         </p>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Submit Videos</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Social media</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3"></i>
+            </Link>
+          </div>
+          <small className="website-rights">TRVL &#169; 2020</small>
+          <div className="social-icons">
+            <Link
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+              className="social-icon-link facebook">
+              <i className="fab fa-facebook-f"></i>
+            </Link>
+            <Link
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram">
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              class="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube">
+              <i class="fab fa-youtube" />
+            </Link>
+            <Link
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter">
+              <i class="fab fa-twitter" />
+            </Link>
+            <Link
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn">
+              <i class="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
       </section>
     </div>
   );
