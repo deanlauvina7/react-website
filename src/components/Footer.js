bf63d7f4a341c5d2648a1d4e17064ba02accf9f4
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-05 18:29:14 -0600

    Started the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Button } from './Button';
 
 function Footer() {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the adventure newsletter to receive our best vacation deals!
+        </p>
+        <p className="footer-subscription-text">
+          You can subscribe at any time.
+          <div className="input-areas">
+            <form>
+              <input
+                type="email"
+                name="email"
+                className="footer-input"
+                placeholder="Your email"
+              />
+              <Button buttonStyle="btn--outline">Subscribe</Button>
+            </form>
+          </div>
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default Footer;
